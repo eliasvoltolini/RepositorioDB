7b243c3673d6e9017a5d34229ac52a3824b0a7f4
Elias Voltolini <88043157+eliasvoltolini@users.noreply.github.com>
2021-08-07 18:50:00 -0300

    Add files via upload

---
+++ RepositorioDB/Model/Interface/UInterface.Model.Interfaces.pas
@@ -0,0 +1,24 @@
+unit UInterface.Model.Interfaces;
+
+interface
+
+type
+  iRepositorioDB = interface
+    ['{139D884E-27F5-403D-A3A6-BC4C0A2D55F2}']
+    function Tabela(Nome: String): iRepositorioDB;
+    function AddCampo(Nome: String): iRepositorioDB; overload;
+    function AddCampo(Nome, Valor: String): iRepositorioDB; overload;
+    function AddWhere(Campo, Valor: String; Condicao: String = '='): iRepositorioDB;
+    function AddGroup(Campo: String): iRepositorioDB;
+    function AddOrder(Campo: String): iRepositorioDB;
+    function Limpa(): iRepositorioDB;
+
+    function AsSelect(): String;
+    function AsInsert(): String;
+    function AsUpdate(): String;
+    function AsDelete(): String;
+  end;
+
+implementation
+
+end.
